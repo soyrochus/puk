d8094024214640d355e23638625a1cda612b1dbf
Iwan van der Kleijn <iwanvanderkleijn@gmail.com>
2026-02-05 06:54:16 +0100

    Reset

--- src/puk/__init__.py
+++
@@ -1,5 +0,0 @@
-"""PUK package."""
-
-__all__ = ["__version__"]
-
-__version__ = "0.1.0"
--- src/puk/tools/__init__.py
+++
@@ -1,7 +0,0 @@
-from .python_exec import create_python_tools
-from .user_io import create_user_io_tools
-
-__all__ = [
-    "create_python_tools",
-    "create_user_io_tools",
-]
